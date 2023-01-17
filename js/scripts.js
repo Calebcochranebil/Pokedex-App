8fa209298ecb883d457f00b8229fc9d534c8b3d2
Caleb Cochrane <Calebcochranebil33@gmail.com>
2023-01-17 16:29:10 -0700

    updated-variables

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+alert('Hello world');
+
+let favoriteFood = 'Pizza';
+document.write(favoriteFood);
+
+let size = 100;
+let doubleSize = size * 2;
+let minSize = (doubleSize * 2) - (size / 2);
+document.write(minSize);
