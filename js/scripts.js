7abba9835fe43058f8d446800c8038720f7a6e2a
Caleb Cochrane <Calebcochranebil33@gmail.com>
2023-01-25 12:14:17 -0700

    added-commetns-IIFE

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,38 @@
-let pokemonList = [{
-    name: 'Nidoking',
-    type: ['Ground','Poison'],
-    height: 1.6
-    },{
-    name: 'Golbat',
-    type: ['Poison','Flying'],
-    height: 1.6
-    },{
-    name: 'Arbok', 
-    type: ['Poison'], 
-    height: 3.5
-    },{
-    name: 'Charizard',
-    type: ['Fire','Flying'],
-    height: 5.7
-    }];
-
-pokemonList.forEach(function(pokemon) {
-    if (pokemon.height >=5) { // If the pokemon height is greater than or equal to 5
-      document.write(pokemon.name + " (height: " + pokemon.height + "m) - Wow, that is a really big Pokemon!!" + "<br>")
-    } else if (pokemon.height >= 3 && pokemon.height < 4.9) { // If the pokemon list is great than 3 but less than 4.9
-      document.write(pokemon.name + " (height: " + pokemon.height + "m) - That is an average sized Pokemon!" + "<br>")
-    } else { // If the first 2 are false this is true
-      document.write(pokemon.name + " (height: " + pokemon.height + "m) - That is a small pokemon." + "<br>")
-    }
+let pokemonRepository = (function() {
+  // This declares the pokemonList array
+  var pokemonList = [{
+  name: 'Golbat',
+  type: ['Poison','Flying'],
+  height: 1.6
+  },{
+  name: 'Arbok',
+  type: ['Poison'],
+  height: 3.5
+  },{
+  name: 'Charizard',
+  type: ['Fire','Flying'],
+  height: 5.7
+  }];
+  
+  return {
+  getAll: function() {
+  // This returns all items in the pokemonList array
+  return pokemonList;
+  },
+  add: function(pokemon) {
+  // This adds a single item to the pokemonList array
+  pokemonList.push(pokemon);
+  }
+  };
+  })();
+  
+  let allPokemon = pokemonRepository.getAll();
+  allPokemon.forEach(function(pokemon) {
+  if (pokemon.height >=5) { // If the height is greater than 5, this gets called
+  document.write(pokemon.name + " (height: " + pokemon.height + "m) - Wow, that is a really big Pokemon!!" + "<br>")
+  } else if (pokemon.height >= 3 && pokemon.height < 4.9) { // If the height is greater than 3 and less than 4.9, this gets called
+  document.write(pokemon.name + " (height: " + pokemon.height + "m) - That is an average sized Pokemon!" + "<br>")
+  } else { // If the height isanything less, this gets called
+  document.write(pokemon.name + " (height: " + pokemon.height + "m) - That is a small pokemon." + "<br>")
+  }
   });
