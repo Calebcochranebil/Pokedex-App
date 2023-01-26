bf3553354c2fb4c66b76de57696601b1fca360f8
Caleb Cochrane <Calebcochranebil33@gmail.com>
2023-01-26 09:36:36 -0700

    update

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,17 @@
 let pokemonRepository = (function() {
   // This declares the pokemonList array
-  var pokemonList = [{
-  name: 'Golbat',
-  type: ['Poison','Flying'],
-  height: 1.6
+  let pokemonList = [{
+    name: 'Golbat',
+    type: ['Poison','Flying'],
+    height: 1.6
   },{
-  name: 'Arbok',
-  type: ['Poison'],
-  height: 3.5
+    name: 'Arbok',
+    type: ['Poison'],
+    height: 3.5
   },{
-  name: 'Charizard',
-  type: ['Fire','Flying'],
-  height: 5.7
+    name: 'Charizard',
+    type: ['Fire','Flying'],
+    height: 5.7
   }];
   
     
@@ -31,11 +31,11 @@ let pokemonRepository = (function() {
   
   let allPokemon = pokemonRepository.getAll();
   allPokemon.forEach(function(pokemon) {
-  if (pokemon.height >=5) { // If the height is greater than 5, this gets called
-  document.write(pokemon.name + " (height: " + pokemon.height + "m) - Wow, that is a really big Pokemon!!" + "<br>")
-  } else if (pokemon.height >= 3 && pokemon.height < 4.9) { // If the height is greater than 3 and less than 4.9, this gets called
-  document.write(pokemon.name + " (height: " + pokemon.height + "m) - That is an average sized Pokemon!" + "<br>")
-  } else { // If the height isanything less, this gets called
-  document.write(pokemon.name + " (height: " + pokemon.height + "m) - That is a small pokemon." + "<br>")
+    if (pokemon.height >=5) { // If the height is greater than 5, this gets called
+    document.write(pokemon.name + " (height: " + pokemon.height + "m) - Wow, that is a really big Pokemon!!" + "<br>")
+    } else if (pokemon.height >= 3 && pokemon.height < 4.9) { // If the height is greater than 3 and less than 4.9, this gets called
+    document.write(pokemon.name + " (height: " + pokemon.height + "m) - That is an average sized Pokemon!" + "<br>")
+    } else { // If the height isanything less, this gets called
+    document.write(pokemon.name + " (height: " + pokemon.height + "m) - That is a small pokemon." + "<br>")
   }
-  });
+});
